7ee55914b13d9a62d52b70885013bad3beeb40e3
Fjayala333 <83990325+Fjayala333@users.noreply.github.com>
2021-08-21 17:01:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project--6-game_show_app
+ 
---
+++ app.js
@@ -0,0 +1,18 @@
+const startGame = document.querySelector('.btn_reset');
+const keyboard = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const lives = document.querySelectorAll('.tries');
+const overlay = document.getElementById('overlay');
+let missed = 0;
+
+const phrases = [
+    "Let It Go",
+    "Show Yourself",
+    "You're Welcome",
+    "Un Poco Loco",
+    "Be Our Guest"
+];
+
+startGame.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
